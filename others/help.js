ad4bc864640e5936d3781b217470bf60a0517d91
r411n <89973489+r411n@users.noreply.github.com>
2021-09-04 21:42:08 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,7 +18,7 @@ module.exports = {
     .setImage(`https://cdn.discordapp.com/attachments/832678897434230864/833341017125683200/standard_15.gif`)
     .setColor("#FC00FF")
     .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
-    .setAuthor("Remix Bot ✨", "https://cdn.discordapp.com/avatars/792047204361175091/c3c061050f56e58aa1f008de8b86dd26.png?size=1024")
+    .setAuthor("Lilaaaa ✨", "https://cdn.discordapp.com/avatars/792047204361175091/c3c061050f56e58aa1f008de8b86dd26.png?size=1024")
     .setDescription(` 
 <a:discord:813406342173818910> **| User Commands**
 > \`invite . invites . support . about   avatar . ping . prefix . uptime  . vote . se . serverinfo   servericon(icon) . userinfo(user)   emojify . emojiinfo(emoji) . social   membercount(count)\`
